d8be4927c837e65f8474ea0e44da31d8c0511d1d
jIAnZhong <eddie@kooco.com.tw>
2013-12-27 16:20:04 +0800

    C#

--- AzDG.cs
+++ AzDG.cs
@@ -26,51 +26,39 @@ namespace jIAnSoft.Framework.Security.Cryptography {
                         DateTime.Now.Ticks.ToString(
                             CultureInfo.InvariantCulture)).ToLower());
             var abteReturn = new byte[argStrSource.Count * 2];
-
-            for (int i = 0, j = 0; i < argStrSource.Count; ++i, ++j) {
-                abteReturn[j] = abteCrcKey[i % 32];
-                ++j;
-                abteReturn[j] = (byte)(argStrSource[i] ^ abteCrcKey[i % 32]);
+            for (var j = 0; j < argStrSource.Count; j++) {
+                abteReturn[(j * 2)] = abteCrcKey[j % 32];
+                abteReturn[(j * 2) + 1] = (byte)(argStrSource[j] ^ abteCrcKey[j % 32]);
             }
-
             return PrivateKeyCrypt(abteReturn);
         }
 
-        public static String Encrypt(String argStrSource) {
-            return Encrypt(argStrSource, "");
-        }
-
-        public static String Encrypt(String argStrSource, String argStrPrivateKey) {
+        public static String Encrypt(String argStrSource, String argStrPrivateKey = "") {
             return Encrypt(argStrSource, argStrPrivateKey, _strCharSet);
         }
 
         public static String Encrypt(String argStrSource, String argStrPrivateKey, String argStrCharset) {
             _strCharSet = argStrCharset;
-            _strGenuineKey = StrCompanyKey + argStrPrivateKey;
+            _strGenuineKey = string.Format("{0}{1}", StrCompanyKey, argStrPrivateKey);
             return Convert.ToBase64String(DoEncrypt(Encoding.GetEncoding(_strCharSet).GetBytes(argStrSource)));
         }
 
         private static byte[] DoDecrypt(byte[] argStrSource) {
             argStrSource = PrivateKeyCrypt(argStrSource);
             var abteReturn = new byte[argStrSource.Length / 2];
-            for (int i = 0, j = 0; i < argStrSource.Length; ++i, ++j) {
-                abteReturn[j] = (byte)(argStrSource[i] ^ argStrSource[++i]);
+            for (var j = 0; j < abteReturn.Length; ++j) {
+                abteReturn[j] = (byte)(argStrSource[(j * 2)] ^ argStrSource[(j * 2) + 1]);
             }
             return abteReturn;
         }
 
-        public static String Decrypt(String argStrSource) {
-            return Decrypt(argStrSource, "");
-        }
-
-        public static String Decrypt(String argStrSource, String argStrPrivateKey) {
+        public static String Decrypt(String argStrSource, String argStrPrivateKey = "") {
             return Decrypt(argStrSource, argStrPrivateKey, _strCharSet);
         }
 
         public static String Decrypt(String argStrSource, String argStrPrivateKey, String argStrCharset) {
-            _strCharSet = argStrCharset;
             _strGenuineKey = string.Format("{0}{1}", StrCompanyKey, argStrPrivateKey);
-            return Encoding.GetEncoding(_strCharSet).GetString(DoDecrypt(Convert.FromBase64String(argStrSource)));
+            return Encoding.GetEncoding(argStrCharset).GetString(DoDecrypt(Convert.FromBase64String(argStrSource)));
         }
     }
 }
